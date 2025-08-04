9935fcd7e56576bb89fd6215616e91255c22599f
trana261003 <130343068+trana261003@users.noreply.github.com>
2025-08-04 15:12:33 +0530

    Update Ledger.js

--- models/Ledger.js
+++ models/Ledger.js
@@ -1,1 +1,27 @@
+// models/Ledger.js
+const mongoose = require('mongoose');
 
+const entrySchema = new mongoose.Schema({
+  date: String,
+  // amount: Number
+  amount: {
+    type: Number,
+    required: true
+  }
+});
+
+const ledgerSchema = new mongoose.Schema({
+  personName: String,
+  bankName: String,
+  ledgerName: String,
+  type: { type: String, enum: ['Credit', 'Debit'] },
+  year: String, // e.g., "2023-24"
+  entries: [entrySchema],
+  // total: Number
+  total: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Ledger', ledgerSchema);
