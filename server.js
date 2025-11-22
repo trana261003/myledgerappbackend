dd268be8159455299fc3e7416d7844b0328521f1
trana261003 <130343068+trana261003@users.noreply.github.com>
2025-11-22 17:57:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1);
   });
 
+app.use("/auth", require("./routes/authRoutes"));
 app.use('/api', ledgerRoutes);
 
 
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
