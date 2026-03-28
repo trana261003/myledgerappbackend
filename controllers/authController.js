3e690b9d81d9aff35b4d8a83a89af996de2cd354
trana261003 <130343068+trana261003@users.noreply.github.com>
2026-03-28 10:36:28 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// REGISTER USER
+// REGISTER
 exports.registerUser = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -13,14 +13,14 @@ exports.registerUser = async (req, res) => {
     const hashed = await bcrypt.hash(password, 10);
 
     await User.create({ username, password: hashed });
-    res.json({ msg: "Registered Successfully" });
 
-  } catch (err) {
+    res.json({ msg: "Registered Successfully" });
+  } catch {
     res.status(500).json({ msg: "Server Error" });
   }
 };
 
-// LOGIN USER
+// LOGIN
 exports.loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -38,8 +38,55 @@ exports.loginUser = async (req, res) => {
     );
 
     res.json({ msg: "Login Success", token });
-
-  } catch (err) {
+  } catch {
     res.status(500).json({ msg: "Server Error" });
   }
 };
+
+
+// =============================== old working ===================================
+// const User = require("../models/User");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+
+// // REGISTER USER
+// exports.registerUser = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     const exist = await User.findOne({ username });
+//     if (exist) return res.status(400).json({ msg: "User already exists" });
+
+//     const hashed = await bcrypt.hash(password, 10);
+
+//     await User.create({ username, password: hashed });
+//     res.json({ msg: "Registered Successfully" });
+
+//   } catch (err) {
+//     res.status(500).json({ msg: "Server Error" });
+//   }
+// };
+
+// // LOGIN USER
+// exports.loginUser = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     const user = await User.findOne({ username });
+//     if (!user) return res.status(400).json({ msg: "User not found" });
+
+//     const match = await bcrypt.compare(password, user.password);
+//     if (!match) return res.status(400).json({ msg: "Wrong password" });
+
+//     const token = jwt.sign(
+//       { id: user._id, username: user.username },
+//       "SECRET_KEY",
+//       { expiresIn: "7d" }
+//     );
+
+//     res.json({ msg: "Login Success", token });
+
+//   } catch (err) {
+//     res.status(500).json({ msg: "Server Error" });
+//   }
+// };
