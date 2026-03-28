9f26d326349d2ab49c1eb1b1e7d79a4e2bd9cf00
trana261003 <130343068+trana261003@users.noreply.github.com>
2026-03-28 10:34:19 +0530

    Update Ledger.js

--- models/Ledger.js
+++ models/Ledger.js
@@ -1,9 +1,7 @@
-// models/Ledger.js
 const mongoose = require('mongoose');
 
 const entrySchema = new mongoose.Schema({
   date: String,
-  // amount: Number
   amount: {
     type: Number,
     required: true
@@ -11,13 +9,17 @@ const entrySchema = new mongoose.Schema({
 });
 
 const ledgerSchema = new mongoose.Schema({
+  userId: {   // ✅ NEW FIELD
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
   personName: String,
   bankName: String,
   ledgerName: String,
   type: { type: String, enum: ['Credit', 'Debit'] },
-  year: String, // e.g., "2023-24"
+  year: String,
   entries: [entrySchema],
-  // total: Number
   total: {
     type: Number,
     required: true
@@ -25,3 +27,34 @@ const ledgerSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Ledger', ledgerSchema);
+
+
+
+// ============================old working ========================
+// // models/Ledger.js
+// const mongoose = require('mongoose');
+
+// const entrySchema = new mongoose.Schema({
+//   date: String,
+//   // amount: Number
+//   amount: {
+//     type: Number,
+//     required: true
+//   }
+// });
+
+// const ledgerSchema = new mongoose.Schema({
+//   personName: String,
+//   bankName: String,
+//   ledgerName: String,
+//   type: { type: String, enum: ['Credit', 'Debit'] },
+//   year: String, // e.g., "2023-24"
+//   entries: [entrySchema],
+//   // total: Number
+//   total: {
+//     type: Number,
+//     required: true
+//   }
+// });
+
+// module.exports = mongoose.model('Ledger', ledgerSchema);
